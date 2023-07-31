e0b79b2338983803f11cea27871b3f893d8ebd81
Jayanta <jmandal82@hotmail.com>
2023-08-01 03:32:22 +0530

    Corrected spelling in db-script

--- src/main/resources/db-scripts/db-insert-script-01.sql
+++ src/main/resources/db-scripts/db-insert-script-01.sql
@@ -5,11 +5,11 @@ INSERT INTO `univeducation`.`departments`
 research_areas,
 num_of_students, num_of_teaching_staffs, num_of_non_teaching_stf)
 VALUES
-('Computer Science & Enginnering',
+('Computer Science & Engineering',
 'CS',
-'BTech, MTech, Dual Deegree, PhD, MS',
+'BTech, MTech, Dual Degree, PhD, MS',
 '1989-12-12',
-'Artificial Intelligence, Machine Learning, Game Theory Application, High Performance Computer Architechture',
+'Artificial Intelligence, Machine Learning, Game Theory Application, High Performance Computer Architecture',
 450, 40, 80);
 
 
@@ -32,7 +32,7 @@ Insert into `students` (
   phone_number, parent_name, par_contact_num )
 Values (
     '01CS3003', 'Rahul Dev Singh', '1983-05-13', 'CS', '561290878799',
-    'Bhillai, Chattisgarh',
+    'Bhillai, Chhattisgarh',
     'RP Hall, Room C-305, Kharagpur',
     '78713342247', 'Mr Birbal', '065621212'
 );
